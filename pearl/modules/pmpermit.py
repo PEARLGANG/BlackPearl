5c678a6cef17174792d4b5e8bbc80b6d85c3a474
PEARLGANG <73771895+PEARLGANG@users.noreply.github.com>
2020-11-01 15:28:50 +0530

    Update pmpermit.py

--- pearl/modules/pmpermit.py
+++ pearl/modules/pmpermit.py
@@ -31,6 +31,8 @@ USER_BOT_WARN_ZERO = "You Have Attempted To Spam Masters Inbox So Inorder To Avo
 botisnoob = Var.TG_BOT_USER_NAME_BF_HER
 USER_BOT_NO_WARN = (
     "**Hello, This is Black Pearl PM Protection Service ⚠️**\n\n"
+    "**Hahah!! Caught u there,  wanna chat with my master** 😏 \n\n"
+    "**Then wait or if u tried to spam then make it sure I'll gonna be blocked and no one gonna daymn give u a fuck** \n\n"
     f"`My Master {DEFAULTUSER} is Busy Right Now !` \n"
     "**I Request You To Choose A Reason You Have Came For** 👀 \n\n"
     f"**{CUSTOM_MIDDLE_PMP}**")
