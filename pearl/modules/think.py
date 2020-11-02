95d7e204325a96f8bd1751ab6560e5ae66615a19
PEARLGANG <73771895+PEARLGANG@users.noreply.github.com>
2020-11-02 22:05:13 +0530

    Update think.py

--- pearl/modules/think.py
+++ pearl/modules/think.py
@@ -14,7 +14,7 @@ from pearl.utils import pearl_on_cmd
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 0.1
+    animation_interval = 0.5
     animation_ttl = range(0, 288)
 
     # await event.edit(input_str)
