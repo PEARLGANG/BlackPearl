23c6f115a8c27d5c65b685e3bc3879b60c178039
PEARLGANG <73771895+PEARLGANG@users.noreply.github.com>
2020-11-02 16:13:25 +0530

    Update ok.py

--- pearl/modules/ok.py
+++ pearl/modules/ok.py
@@ -14,7 +14,7 @@ from pearl.utils import pearl_on_cmd
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 0.00001
+    animation_interval = 0.5
     animation_ttl = range(0, 90)
     input_str = event.pattern_match.group(1)
     if input_str == "k":
