88611e897316c0acc6ee778ba85549c2356c5174
PEARLGANG <73771895+PEARLGANG@users.noreply.github.com>
2020-11-04 17:18:22 +0530

    Update _inlinebot.py

--- pearl/modules/_inlinebot.py
+++ pearl/modules/_inlinebot.py
@@ -27,7 +27,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
-                "© Userbot Help",
+                "© Black Pearl Help",
                 text="{}\nCurrently Loaded Plugins: {}".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
