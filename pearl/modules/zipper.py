3e34e9c4e5e359cc78d248d84343c16141c94175
PEARLGANG <73771895+PEARLGANG@users.noreply.github.com>
2020-11-12 10:35:59 +0530

    Update and rename unzipfile.py to zipper.py

--- pearl/modules/zipper.py
+++ pearl/modules/zipper.py
@@ -1,16 +1,43 @@
-""" command: .unzip
+""" command: .unzip , .zip
 """
-
-import os
-import time
-import zipfile
+from telethon import events, types
 from datetime import datetime
-
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from telethon.tl.types import DocumentAttributeVideo
-
 from pearl.utils import pearl_on_cmd
+import os, asyncio, zipfile, time, json, requests, aiohttp, pytz, math, io
+from PIL import Image
+from pySmartDL import SmartDL
+from datetime import datetime, date
+from telegraph import exceptions
+from pearl import (TEMP_DOWNLOAD_DIRECTORY, BOTLOG_CHATID, CMD_HELP)
+from pearl.events import pearl_on_cmd
+from io import BytesIO
+from asyncio import sleep
+from telethon.tl import functions, types
+from pearl.events import progress
+thumb_image_path = TEMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
+extracted = TEMP_DOWNLOAD_DIRECTORY + "extracted/"
+if not os.path.isdir(extracted):
+    os.makedirs(extracted)
+ZIP_DOWNLOAD_DIRECTORY = TEMP_DOWNLOAD_DIRECTORY
+import time as t
+x = math.inf
+counter = 0
+start=t.time()
+today = date.today()
+
+
+
+def zipdir(path, ziph):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+            os.remove(os.path.join(root, file))
+
+
+
 
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
 extracted = Config.TMP_DOWNLOAD_DIRECTORY + "extracted/"
@@ -112,3 +139,46 @@ def get_lst_of_files(input_directory, output_lst):
             return get_lst_of_files(current_file_name, output_lst)
         output_lst.append(current_file_name)
     return output_lst
+
+@pearl.on(pearl_on_cmd(pattern=f"zip$"))
+async def _(event):
+    sender = await event.get_sender() ; me = await event.client.get_me()
+    if not sender.id == me.id:
+        rkp = await event.reply("`processing`")
+    else:
+    	rkp = await event.edit("`processing`")
+    if event.fwd_from:
+        return
+    if not event.is_reply:
+        await rkp.edit("Reply to a file to compress it.")
+        return
+    mone = await rkp.edit("Processing ...")
+    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        try:
+            c_time = t.time()
+            downloaded_file_name = await bot.download_media(
+                reply_message,
+                TEMP_DOWNLOAD_DIRECTORY,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, mone, c_time, "trying to download")
+                )
+            )
+            directory_name = downloaded_file_name
+            await rkp.edit(downloaded_file_name)
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await rkp.edit(str(e))
+    zipfile.ZipFile(directory_name + '.zip', 'w', zipfile.ZIP_DEFLATED).write(directory_name)
+    await bot.send_file(
+        event.chat_id,
+        directory_name + ".zip",
+        caption="Zipped ",
+        force_document=True,
+        allow_cache=False,
+        reply_to=event.message.id,
+    )
+    await rkp.edit("DONE!!!")
+    await asyncio.sleep(7)
+    await event.delete()
