792ae828be847352174e774b54fcc57444976eff
PEARLGANG <73771895+PEARLGANG@users.noreply.github.com>
2020-11-13 20:33:45 +0530

    Add files via upload

---
+++ pearl/modules/glitchhh.py
@@ -0,0 +1,117 @@
+import asyncio
+import os
+import random
+import re
+import textwrap
+import time
+
+from glitch_this import ImageGlitcher
+from PIL import Image, ImageDraw, ImageFont
+from telethon import events, functions, types
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from pearl import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
+from pearl.events import register
+from pearl.utils import check_media, progress
+
+Glitched = TEMP_DOWNLOAD_DIRECTORY + "glitch.gif"
+
+EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats
+    "]+"
+)
+
+
+@register(outgoing=True, pattern=r"^\.glitch(?: |$)(.*)")
+async def glitch(event):
+    if not event.reply_to_msg_id:
+        await event.edit("`I Wont Glitch A Ghost!`")
+        return
+    reply_message = await event.get_reply_message()
+    if not reply_message.media:
+        await event.edit("`reply to a image/sticker`")
+        return
+    await bot.download_file(reply_message.media)
+    await event.edit("`Downloading Media..`")
+    if event.is_reply:
+        data = await check_media(reply_message)
+        if isinstance(data, bool):
+            await event.edit("`Unsupported Files...`")
+            return
+    else:
+        await event.edit("`Reply to Any Media Sur`")
+        return
+
+    try:
+        value = int(event.pattern_match.group(1))
+        if value > 8:
+            raise ValueError
+    except ValueError:
+        value = 2
+    await event.edit("```Glitching This Media```")
+    await asyncio.sleep(2)
+    file_name = "glitch.png"
+    to_download_directory = TEMP_DOWNLOAD_DIRECTORY
+    downloaded_file_name = os.path.join(to_download_directory, file_name)
+    downloaded_file_name = await bot.download_media(
+        reply_message,
+        downloaded_file_name,
+    )
+    glitch_file = downloaded_file_name
+    glitcher = ImageGlitcher()
+    img = Image.open(glitch_file)
+    glitch_img = glitcher.glitch_image(img, value, color_offset=True, gif=True)
+    DURATION = 200
+    LOOP = 0
+    glitch_img[0].save(
+        Glitched,
+        format="GIF",
+        append_images=glitch_img[1:],
+        save_all=True,
+        duration=DURATION,
+        loop=LOOP,
+    )
+    await event.edit("`Uploading Glitched Media...`")
+    c_time = time.time()
+    nosave = await event.client.send_file(
+        event.chat_id,
+        Glitched,
+        force_document=False,
+        reply_to=event.reply_to_msg_id,
+        progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+            progress(d, t, event, c_time, "[UPLOAD]")
+        ),
+    )
+    await event.delete()
+    os.remove(Glitched)
+    await bot(
+        functions.messages.SaveGifRequest(
+            id=types.InputDocument(
+                id=nosave.media.document.id,
+                access_hash=nosave.media.document.access_hash,
+                file_reference=nosave.media.document.file_reference,
+            ),
+            unsave=True,
+        )
+    )
+    os.remove(glitch_file)
+
+
+CMD_HELP.update(
+    {
+        "glitch": ".glitch <1-8>\
+            \nUsage: Reply a sticker/image and send with cmd.\
+            \nvalue is range 1-8 if doenst it will give default value which is 2"
+    }
+)
---
+++ pearl/modules/hazmat.py
@@ -0,0 +1,103 @@
+import asyncio
+import os
+import re
+import textwrap
+import time
+from telethon import events, functions, types
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from pearl import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
+from pearl.events import register
+from pearl.utils import check_media, progress
+
+EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats
+    "]+"
+)
+
+
+@register(outgoing=True, pattern=r"^.hz(:? |$)(.*)?")
+async def hazz(hazmat):
+    await hazmat.edit("`Sending information...`")
+    level = hazmat.pattern_match.group(2)
+    if hazmat.fwd_from:
+        return
+    if not hazmat.reply_to_msg_id:
+        await hazmat.edit("`WoWoWo Capt!, we are not going suit a ghost!...`")
+        return
+    reply_message = await hazmat.get_reply_message()
+    if not reply_message.media:
+        await hazmat.edit("`Word can destroy anything Capt!...`")
+        return
+    if reply_message.sender.bot:
+        await hazmat.edit("`Reply to actual user...`")
+        return
+    chat = "@hazmat_suit_bot"
+    await hazmat.edit("```Suit Up Capt!, We are going to purge some virus...```")
+    message_id_to_reply = hazmat.message.reply_to_msg_id
+    msg_reply = None
+    async with hazmat.client.conversation(chat) as conv:
+        try:
+            msg = await conv.send_message(reply_message)
+            if level:
+                m = f"/hazmat {level}"
+                msg_reply = await conv.send_message(m, reply_to=msg.id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            elif reply_message.gif:
+                m = f"/hazmat"
+                msg_reply = await conv.send_message(m, reply_to=msg.id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            else:
+                response = await conv.get_response()
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await hazmat.reply("`Please unblock` @hazmat_suit_bot`...`")
+            return
+        if response.text.startswith("I can't"):
+            await hazmat.edit("`Can't handle this GIF...`")
+            await hazmat.client.delete_messages(
+                conv.chat_id, [msg.id, response.id, r.id, msg_reply.id]
+            )
+            return
+        else:
+            downloaded_file_name = await hazmat.client.download_media(
+                response.media, TEMP_DOWNLOAD_DIRECTORY
+            )
+            await hazmat.client.send_file(
+                hazmat.chat_id,
+                downloaded_file_name,
+                force_document=False,
+                reply_to=message_id_to_reply,
+            )
+            """ - cleanup chat after completed - """
+            if msg_reply is not None:
+                await hazmat.client.delete_messages(
+                    conv.chat_id, [msg.id, msg_reply.id, r.id, response.id]
+                )
+            else:
+                await hazmat.client.delete_messages(conv.chat_id, [msg.id, response.id])
+    await hazmat.delete()
+    return os.remove(downloaded_file_name)
+
+
+CMD_HELP.update(
+    {
+        "hazmat": ".hz or .hz [flip, x2, rotate (degree), background (number), black]"
+        "\nUsage: Reply to a image / sticker to suit up!"
+        "\n@hazmat_suit_bot"
+    }
+)
