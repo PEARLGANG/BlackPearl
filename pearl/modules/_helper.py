7820b0eec127e77413489b0c2c1f81a9f6bfc995
PEARLGANG <73771895+PEARLGANG@users.noreply.github.com>
2020-11-15 18:47:36 +0530

    Update _helper.py

--- pearl/modules/_helper.py
+++ pearl/modules/_helper.py
@@ -29,7 +29,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Black Pearl Modules Are Listed Here !\n
+            help_string = """Black Pearl Features Are Listed Here !\n
+✪༺ ──•◈•───•◈•──༻✪\n
 For More Help or Support Visit @pearlsupport"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
