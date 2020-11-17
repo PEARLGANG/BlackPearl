33b680797f81bb99e328cee37584bf9ebf9c51a9
PEARLGANG <73771895+PEARLGANG@users.noreply.github.com>
2020-11-17 10:42:05 +0530

    Update alive.py

--- pearl/modules/alive.py
+++ pearl/modules/alive.py
@@ -37,8 +37,8 @@ def get_readable_time(seconds: int) -> str:
 uptime = get_readable_time((time.time() - Lastupdate))
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = Config.ALIVE_IMAGE
-pm_caption = "➢ **BLACK PEARL IS:** SAILING\n\n"
-pm_caption += "✪༺ ──•◈•───•◈•──༻✪\n"
+pm_caption = "➢ **BLACK PEARL IS:** SAILING\n"
+pm_caption += "✪༺ ──•◈•───•◈•──༻✪\n\n"
 pm_caption += "➢ **SYSTEMS STATS**\n"
 pm_caption += "➢ **Telethon Version:** `1.15.0` \n"
 pm_caption += "➢ **Python:** `3.7.4` \n"
@@ -47,11 +47,11 @@ pm_caption += "➢ **Database Status:**  `Functional`\n"
 pm_caption += "➢ **Current Branch** : `master`\n"
 pm_caption += f"➢ **Version** : `1.0`\n"
 pm_caption += f"➢ **My Boss** : {DEFAULTUSER} \n"
-pm_caption += "➢ **Heroku Database** : `AWS - STILL WORKS LIKE A CHARM`\n\n"
+pm_caption += "➢ **Heroku Database** : `AWS - WORKS LIKE A CHARM`\n"
 pm_caption += "➢ **License** : [GNU General Public License v3.0](github.com/PEARLGANG/BlackPearl/blob/main/LICENSE)\n"
 pm_caption += "➢ **Copyright** : By [Github](GitHub.com/PEARLGANG)\n"
-pm_caption += "➢ **Check Stats By Doing** `.stat`. \n\n"
-pm_caption += "✪༺ ──•◈•───•◈•──༻✪\n"
+pm_caption += "➢ **Check Stats By Doing** `.stat`. \n"
+pm_caption += "✪༺ ──•◈•───•◈•──༻✪\n\n"
 pm_caption += "➢ **[Deploy Black Pearl](https://heroku.com/deploy?template=https://github.com/PEARLGANG/BlackPearl/tree/main)** \n"
 
 
