72e3c561c9d8147f534951c9376cc82d11c5cd10
PEARLGANG <73771895+PEARLGANG@users.noreply.github.com>
2020-11-23 22:49:41 +0530

    Update updater.py

--- pearl/modules/updater.py
+++ pearl/modules/updater.py
@@ -1,146 +1,194 @@
-"""Syntax: .update
-\nFor this awasome plugin.\nPorted from PpaperPlane Extended"""
+"""
+This module is used for updating BlackPearl
+"""
 
 import asyncio
-import os
 import sys
-from pearl.Configs import Config
-import git
+from os import environ, execle, path, remove
 
-from pearl.utils import pearl_on_cmd
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
-# -- Constants -- #
-IS_SELECTED_DIFFERENT_BRANCH = (
-    "looks like a custom branch {branch_name} "
-    "is being used:\n"
-    "in this case, Updater is unable to identify the branch to be updated."
-    "please check out to an official branch, and re-start the updater."
+from pearl import CMD_HNDLR
+
+requirements_path = path.join(
+    path.dirname(path.dirname(path.dirname(__file__))), "requirements.txt"
 )
-OFFICIAL_UPSTREAM_REPO = Config.UPSTREAM_REPO
-BOT_IS_UP_TO_DATE = "`The Black Pearl is up-to-date.\nThank you for Using this Service.`"
-NEW_BOT_UP_DATE_FOUND = (
-    "New update found for {branch_name}\n" "changelog: \n\n{changelog}\n" "updating ..."
-)
-NEW_UP_DATE_FOUND = "New update found for {branch_name}\n" "`updating ...`"
-REPO_REMOTE_NAME = "temponame"
-IFFUCI_ACTIVE_BRANCH_NAME = "main"
-DIFF_MARKER = "HEAD..{remote_name}/{branch_name}"
-NO_HEROKU_APP_CFGD = "no heroku application found, but a key given? 😕 "
-HEROKU_GIT_REF_SPEC = "HEAD:refs/heads/main"
-RESTARTING_APP = "re-starting heroku application"
-ALL_DONE = (
-    "Woo-Hoo!! We Got New Features, Now Restarting Black Pearl wait for 5 mins.\n\n"
-    "✪༺ ──•◈•───•◈•──༻✪\n\n"
-    "Try `.test` To Test. \n© @pearlsupport"
-)
-# -- Constants End -- #
+
+HEROKU_API_KEY = Var.HEROKU_API_KEY
+HEROKU_APP_NAME = Var.HEROKU_APP_NAME
+GIT_REPO_NAME = "BLackPearl"
+UPSTREAM_REPO_URL = "https://github.com/PEARLGANG/BlackPearl"
+
+rider = CMD_HNDLR if CMD_HNDLR else "."
 
 
-@pearl.on(pearl_on_cmd("..update ?(.*)", outgoing=True))
-async def updater(message):
+async def gen_chlog(repo, diff):
+    ch_log = ""
+    d_form = "On " + "%d/%m/%y" + " at " + "%H:%M:%S"
+    for c in repo.iter_commits(diff):
+        ch_log += f"**#{c.count()}** : {c.committed_datetime.strftime(d_form)} : [{c.summary}]({UPSTREAM_REPO_URL.rstrip('/')}/commit/{c}) by **{c.author}**\n"
+    return ch_log
+
+
+async def updateme_requirements():
+    reqs = str(requirements_path)
     try:
-        repo = git.Repo()
-    except git.exc.InvalidGitRepositoryError as e:
-        repo = git.Repo.init()
-        origin = repo.create_remote(REPO_REMOTE_NAME, OFFICIAL_UPSTREAM_REPO)
-        origin.fetch()
-        repo.create_head(IFFUCI_ACTIVE_BRANCH_NAME, origin.refs.main)
-        repo.heads.main.checkout(True)
-
-    active_branch_name = repo.active_branch.name
-    if active_branch_name != IFFUCI_ACTIVE_BRANCH_NAME:
-        await message.edit(
-            IS_SELECTED_DIFFERENT_BRANCH.format(branch_name=active_branch_name)
+        process = await asyncio.create_subprocess_shell(
+            " ".join([sys.executable, "-m", "pip", "install", "-r", reqs]),
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
         )
-        return False
-
-    try:
-        repo.create_remote(REPO_REMOTE_NAME, OFFICIAL_UPSTREAM_REPO)
+        await process.communicate()
+        return process.returncode
     except Exception as e:
-        print(e)
+        return repr(e)
 
-    temp_upstream_remote = repo.remote(REPO_REMOTE_NAME)
-    temp_upstream_remote.fetch(active_branch_name)
 
-    changelog = generate_change_log(
-        repo,
-        DIFF_MARKER.format(
-            remote_name=REPO_REMOTE_NAME, branch_name=active_branch_name
-        ),
-    )
+@pearl.on(admin_cmd(pattern="update ?(.*)"))
+async def upstream(ups):
+    "For .update command, check if the bot is up to date, update if specified"
+    await ups.edit("`Searching for new updates, if any...`")
+    conf = ups.pattern_match.group(1)
+    off_repo = UPSTREAM_REPO_URL
+    force_updateme = False
 
-    if not changelog:
-        await message.edit("`Let's check what new we got here......`")
-        await asyncio.sleep(8)
+    try:
+        txt = "`Oops.. Updater cannot continue as "
+        txt += "some problems occured`\n\n**LOGTRACE:**\n"
+        repo = Repo()
+    except NoSuchPathError as error:
+        await ups.edit(f"{txt}\n`directory {error} is not found`")
+        repo.__del__()
+        return
+    except GitCommandError as error:
+        await ups.edit(f"{txt}\n`Early failure! {error}`")
+        repo.__del__()
+        return
+    except InvalidGitRepositoryError as error:
+        if conf != "now":
+            await ups.edit(
+                f"**Unfortunately, the directory {error} does not seem to be a git repository.\
+                \nOr Maybe it just needs a sync verification with {GIT_REPO_NAME}\
+            \nBut we can fix that by force updating the userbot using** `{rider}update now`."
+            )
+            return
+        repo = Repo.init()
+        origin = repo.create_remote("upstream", off_repo)
+        origin.fetch()
+        force_updateme = True
+        repo.create_head("master", origin.refs.master)
+        repo.heads.master.set_tracking_branch(origin.refs.master)
+        repo.heads.master.checkout(True)
 
-    message_one = NEW_BOT_UP_DATE_FOUND.format(
-        branch_name=active_branch_name, changelog=changelog
-    )
-    message_two = NEW_UP_DATE_FOUND.format(branch_name=active_branch_name)
-
-    if len(message_one) > 4095:
-        with open("change.log", "w+", encoding="utf8") as out_file:
-            out_file.write(str(message_one))
-        await tgbot.send_message(
-            message.chat_id, document="change.log", caption=message_two
+    ac_br = repo.active_branch.name
+    if ac_br != "master":
+        await ups.edit(
+            f"**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). "
+            "in that case, Updater is unable to identify "
+            "which branch is to be merged. "
+            "Please checkout the official branch of BlackPearl`"
         )
-        os.remove("change.log")
+        repo.__del__()
+        return
+
+    try:
+        repo.create_remote("upstream", off_repo)
+    except BaseException:
+        pass
+
+    ups_rem = repo.remote("upstream")
+    ups_rem.fetch(ac_br)
+
+    changelog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
+
+    if not changelog and not force_updateme:
+        await ups.edit(
+            f"\n`Your BOT is`  **up-to-date**  `with`  **[[{ac_br}]]({UPSTREAM_REPO_URL}/tree/{ac_br})**\n"
+        )
+        repo.__del__()
+        return
+
+    if conf != "now" and not force_updateme:
+        changelog_str = (
+            f"**New UPDATE available for [[{ac_br}]]({UPSTREAM_REPO_URL}/tree/{ac_br}):**\n\n"
+            + "**CHANGELOG**\n\n"
+            + f"{changelog}"
+        )
+        if len(changelog_str) > 4096:
+            await ups.edit("`Changelog is too big, view the file to see it.`")
+            file = open("output.txt", "w+")
+            file.write(changelog_str)
+            file.close()
+            await ups.client.send_file(
+                ups.chat_id,
+                "output.txt",
+                reply_to=ups.id,
+            )
+            remove("output.txt")
+        else:
+            await ups.edit(changelog_str)
+        await ups.respond(f"Do `{rider}update now` to update")
+        return
+
+    if force_updateme:
+        await ups.edit("`Force-Syncing to latest stable userbot code, please wait...`")
     else:
-        await message.edit(message_one)
-
-    temp_upstream_remote.fetch(active_branch_name)
-    repo.git.reset("--hard", "FETCH_HEAD")
-
+        await ups.edit("`Updating userbot, please wait....`")
+    # We're in a Heroku Dyno, handle it's memez.
     if Var.HEROKU_API_KEY is not None:
         import heroku3
 
         heroku = heroku3.from_key(Var.HEROKU_API_KEY)
+        heroku_app = None
         heroku_applications = heroku.apps()
-        if len(heroku_applications) >= 1:
-            if Var.HEROKU_APP_NAME is not None:
-                heroku_app = None
-                for i in heroku_applications:
-                    if i.name == Var.HEROKU_APP_NAME:
-                        heroku_app = i
-                if heroku_app is None:
-                    await message.edit(
-                        "Invalid APP Name. Please set the name of your bot in heroku in the var `HEROKU_APP_NAME.`"
-                    )
-                    return
-                heroku_git_url = heroku_app.git_url.replace(
-                    "https://", "https://api:" + Var.HEROKU_API_KEY + "@"
-                )
-                if "heroku" in repo.remotes:
-                    remote = repo.remote("heroku")
-                    remote.set_url(heroku_git_url)
-                else:
-                    remote = repo.create_remote("heroku", heroku_git_url)
-                asyncio.get_event_loop().create_task(
-                    deploy_start(tgbot, message, HEROKU_GIT_REF_SPEC, remote)
-                )
-
-            else:
-                await message.edit(
-                    "Please create the var `HEROKU_APP_NAME` as the key and the name of your bot in heroku as your value."
-                )
-                return
+        if not Var.HEROKU_APP_NAME:
+            await ups.edit(
+                "`Please set up the HEROKU_APP_NAME variable to be able to update userbot.`"
+            )
+            repo.__del__()
+            return
+        for app in heroku_applications:
+            if app.name == Var.HEROKU_APP_NAME:
+                heroku_app = app
+                break
+        if heroku_app is None:
+            await ups.edit(
+                f"{txt}\n`Invalid Heroku credentials for updating userbot dyno.`"
+            )
+            repo.__del__()
+            return
+        await ups.edit(
+            "`Userbot dyno build in progress, please wait for it to complete.`"
+        )
+        ups_rem.fetch(ac_br)
+        repo.git.reset("--hard", "FETCH_HEAD")
+        heroku_git_url = heroku_app.git_url.replace(
+            "https://", "https://api:" + Var.HEROKU_API_KEY + "@"
+        )
+        if "heroku" in repo.remotes:
+            remote = repo.remote("heroku")
+            remote.set_url(heroku_git_url)
         else:
-            await message.edit(NO_HEROKU_APP_CFGD)
+            remote = repo.create_remote("heroku", heroku_git_url)
+        try:
+            remote.push(refspec="HEAD:refs/heads/master", force=True)
+        except GitCommandError as error:
+            await ups.edit(f"{txt}\n`Here is the error log:\n{error}`")
+            repo.__del__()
+            return
+        await ups.edit("`Successfully Updated!\n" "Restarting, please wait...`")
     else:
-        await message.edit("No heroku api key found in `HEROKU_API_KEY` var")
-
-
-def generate_change_log(git_repo, diff_marker):
-    out_put_str = ""
-    d_form = "%d/%m/%y"
-    for repo_change in git_repo.iter_commits(diff_marker):
-        out_put_str += f"•[{repo_change.committed_datetime.strftime(d_form)}]: {repo_change.summary} <{repo_change.author}>\n"
-    return out_put_str
-
-
-async def deploy_start(tgbot, message, refspec, remote):
-    await message.edit(RESTARTING_APP)
-    await message.edit(ALL_DONE)
-    await remote.push(refspec=refspec)
-    await tgbot.disconnect()
-    os.execl(sys.executable, sys.executable, *sys.argv)
+        # Classic Updater, pretty straightforward.
+        try:
+            ups_rem.pull(ac_br)
+        except GitCommandError:
+            repo.git.reset("--hard", "FETCH_HEAD")
+        await updateme_requirements()
+        await ups.edit(
+            "`Successfully Updated!\n" "Bot is restarting... Wait for a second!`"
+        )
+        # Spin a new instance of bot
+        args = [sys.executable, "-m", "pearl"]
+        execle(sys.executable, *args, environ)
+        return
