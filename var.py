3e8abf18f17582f47050bf9680f0d0546031813e
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-08-08 11:55:20 +0530

    Update var.py

--- var.py
+++ var.py
@@ -2,9 +2,9 @@ import os
 
 ENV = bool(os.environ.get("ENV", False))
 if ENV:
-    from heroku_config import Var as Config
-else:
     from local_config import Development as Config
-
+    
+else:
+    from heroku_config import Var as Config
 
 Var = Config
