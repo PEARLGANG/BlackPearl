665492361db7c88b7a45c73cfc3bfea78fc70803
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-08-08 12:22:39 +0530

    Update var.py

--- var.py
+++ var.py
@@ -1,10 +1,8 @@
-import os
+class Development(Var):
+  APP_ID = 5204354
+  API_HASH = "f99ae24d5cfb143e7d548f0bcc1315a6"
+  ALIVE_NAME = "RnD"
+  STRING_SESSION = "1AZWarzQBu3JiuJg0xmvO35Nlr1Tq8QNLjY16vOH7YDHqmXDKstqNh1NYI0MzWQcKRqXY-WXFKde9SVxGq-kEuIpMkJgNzc-k-CXohYZeXgjVThu0NBJYb7x_J-UvpvMZQ1UZ-MNa_xTr0rBHwnWIfhRdOIRGtXNiS8O8Ae5vD880pMVP3a1s36UGQFGSnRPSftRlmY-Pup41dueTh8JPGE054Q7m-xS8TAnYN5nSVIvG_IZifyWoW6GTYkYl42lVj6GJ2L3m63WWWlKYDsePGt4vZpSNFqWTkgVbZbHVvOU9l3s2xPYgmLuAPrSHfkQHBWd33A_kfxiz-HknsmPCAI4CIhTkY-4="
 
-ENV = bool(os.environ.get("ENV", False))
-if ENV:
-    from local_config import Development as Config
-    
-else:
-    from heroku_config import Var as Config
 
 Var = Config
