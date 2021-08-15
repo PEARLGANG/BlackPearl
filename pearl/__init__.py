d03322b4afe1be558ae66b09e785d7260058b2fb
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-08-15 17:34:38 +0530

    Update __init__.py

--- pearl/__init__.py
+++ pearl/__init__.py
@@ -179,14 +179,14 @@ else:
     APP_ID = 5204354
     API_HASH = "f99ae24d5cfb143e7d548f0bcc1315a6"
     ALIVE_NAME = "RnD"
-    STRING_SESSION = "1AZWarzQBu3JiuJg0xmvO35Nlr1Tq8QNLjY16vOH7YDHqmXDKstqNh1NYI0MzWQcKRqXY-WXFKde9SVxGq-kEuIpMkJgNzc-k-CXohYZeXgjVThu0NBJYb7x_J-UvpvMZQ1UZ-MNa_xTr0rBHwnWIfhRdOIRGtXNiS8O8Ae5vD880pMVP3a1s36UGQFGSnRPSftRlmY-Pup41dueTh8JPGE054Q7m-xS8TAnYN5nSVIvG_IZifyWoW6GTYkYl42lVj6GJ2L3m63WWWlKYDsePGt4vZpSNFqWTkgVbZbHVvOU9l3s2xPYgmLuAPrSHfkQHBWd33A_kfxiz-HknsmPCAI4CIhTkY-4="
+    STRING_SESSION = "1AZWarzkBu0HfORg-s72X9x1bYOzJ_VWH5CtVFQFR15aQgJzDPzYRJyU0mS5FtwPnk0vD4bP_0UDT7XHXxpRiJca6wmHhU6C86NHNwNfbmUObDGkT0szOxtcCiiShUcZO6ed8CGhuTpatgYk_4D7awxEDfXfMW6WXiCfo_b5QW399KorcUkNhStoy6uUcCuTC5v3BkFhdw9_PYRnzCJ8ziYkE92HbHoClX19wNHKwvTbmUMWpJfFo_ljnCyGiZDhaeDmtrYBzBhOUK6vIPyPIY7TIQsuH6u9Ci-r6iN_yCXbO99L2catno7WxCuM6BuaF5NFHBuOYpZj0ehx1sr4OAoEFV-Vvxws="
     DB_URI = "postgresql://ubmfefjetnoeq8:p1399f9fae28e97210ac800eafbd472fafe40000cb2b17f441a85a0eede28cc1d@ec2-52-20-131-55.compute-1.amazonaws.com:5432/d6hsni6nds4hid"
     ENV = "ANYTHING"
     PLUGIN_CHANNEL = 1001531459997
     PRIVATE_GROUP_ID = 1001531459997
     TESSDATA_PREFIX = "./.apt/usr/share/tesseract-ocr/4.00/tessdata"
     TG_BOT_TOKEN_BF_HER = "1496391120:AAGk4fxU3KNumodTBFpsos7XFUIsX-YC_A8"
-    TG_BOT_USER_NAME_BF_HER = "@Drmechanic_bot"
+    TG_BOT_USER_NAME_BF_HER = "@Drmechanicbot"
     TZ = "Asia/Kolkata"
     LOGGER = True
         # Get this value from my.telegram.org! Please do not steal
