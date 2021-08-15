ca23f7b5b30428b9a500a0ac267ab4162479dc97
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-08-15 19:07:43 +0530

    Update __main__.py

--- pearl/__main__.py
+++ pearl/__main__.py
@@ -10,8 +10,6 @@ from pearl.Configs import Config
 from pearl.utils import load_module, start_assistant
 from var import Var
 
-rider = logging.getLogger("BlackPearl")
-
 
 async def add_bot(bot_token):
     await bot.start(bot_token)
@@ -24,10 +22,15 @@ if len(argv) not in (1, 3, 4):
 else:
     bot.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
+        print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
             "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
+        print("Initialisation finished with no errors")
+        print("Starting To Install Inline In Bot")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
     else:
         bot.start()
 
@@ -40,9 +43,17 @@ for name in files:
         path1 = Path(f.name)
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
-        
-rider.info("Black Pearl Has Been Installed Sucessfully !")
-rider.info("You Can Visit @pearlsupport For Any Support Or Doubts")
+       
+path = "pearl/modules/assistant/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        start_assistant(shortname.replace(".py", ""))
+
+print("Black Pearl And Assistant Bot Have Been Installed Successfully !")
+print("You Can Visit @BlackPearlOT For Any Support Or Doubts")        
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
