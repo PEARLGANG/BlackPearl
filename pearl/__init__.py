3d2a5fe1e87ee9116724890b09bb72b0eb8f9f41
DRMECHANIC <73771895+PEARLGANG@users.noreply.github.com>
2021-08-15 19:34:17 +0530

    Update __init__.py

--- pearl/__init__.py
+++ pearl/__init__.py
@@ -176,18 +176,6 @@ if bool(ENV):
 else:
     # Put your ppe vars here if you are using local hosting
     PLACEHOLDER = None
-    APP_ID = 5204354
-    API_HASH = "f99ae24d5cfb143e7d548f0bcc1315a6"
-    ALIVE_NAME = "RnD"
-    STRING_SESSION = "1AZWarzkBu0HfORg-s72X9x1bYOzJ_VWH5CtVFQFR15aQgJzDPzYRJyU0mS5FtwPnk0vD4bP_0UDT7XHXxpRiJca6wmHhU6C86NHNwNfbmUObDGkT0szOxtcCiiShUcZO6ed8CGhuTpatgYk_4D7awxEDfXfMW6WXiCfo_b5QW399KorcUkNhStoy6uUcCuTC5v3BkFhdw9_PYRnzCJ8ziYkE92HbHoClX19wNHKwvTbmUMWpJfFo_ljnCyGiZDhaeDmtrYBzBhOUK6vIPyPIY7TIQsuH6u9Ci-r6iN_yCXbO99L2catno7WxCuM6BuaF5NFHBuOYpZj0ehx1sr4OAoEFV-Vvxws="
-    DB_URI = "postgresql://ubmfefjetnoeq8:p1399f9fae28e97210ac800eafbd472fafe40000cb2b17f441a85a0eede28cc1d@ec2-52-20-131-55.compute-1.amazonaws.com:5432/d6hsni6nds4hid"
-    ENV = "ANYTHING"
-    PLUGIN_CHANNEL = 1001531459997
-    PRIVATE_GROUP_ID = 1001531459997
-    TESSDATA_PREFIX = "./.apt/usr/share/tesseract-ocr/4.00/tessdata"
-    TG_BOT_TOKEN_BF_HER = "1496391120:AAGk4fxU3KNumodTBFpsos7XFUIsX-YC_A8"
-    TG_BOT_USER_NAME_BF_HER = "@Drmechanicbot"
-    TZ = "Asia/Kolkata"
     LOGGER = True
         # Get this value from my.telegram.org! Please do not steal
     LOCATION = os.environ.get("LOCATION", None)
